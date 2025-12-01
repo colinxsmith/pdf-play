ebc29a3b2b83ef97264b85300b80192bb23e1719
Dr Colin <colin@colin>
2025-12-01 14:55:46 +0000

    .

--- src/services/pdfprint/pdfprint.ts
+++ src/services/pdfprint/pdfprint.ts
@@ -9,13 +9,13 @@ import { PdfSave } from '../../services/pdfsave';
   styleUrls: ['./pdfprint.scss'],
 })
 export class pdfprint {
-  @Input() divelement: HTMLDivElement | null = null;
+  @Input() printarea: HTMLDivElement | null = null;
   @Input() pdfname: string = 'document.pdf';
   @Input() buttontext: string = 'Export to PDF';
   constructor(private pdfsave: PdfSave) { }
   async newpdf(): Promise<void> { 
     try {
-      await this.pdfsave.exportToPdf(this.divelement, this.pdfname, 10, 300);
+      await this.pdfsave.exportToPdf(this.printarea, this.pdfname, 10, 300);
     } catch (error) {
       console.error('PDF export failed:', error);
     }
